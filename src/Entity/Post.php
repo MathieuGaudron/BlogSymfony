4a2075b2cd65a024e292f048b99649ab36f4dafe
mathieu <mathieu.gaudron@epitech.eu>
2026-02-11 10:58:20 +0100

    entity OK

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,42 +2,58 @@
 
 namespace App\Entity;
 
-use App\Repository\PostRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: PostRepository::class)]
-#[ORM\Table(name: 'post')]
+#[ORM\Entity]
 class Post
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: Types::INTEGER)]
+    #[ORM\Column]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
-    #[ORM\Column(type: Types::TEXT)]
+    #[ORM\Column(type: 'text')]
     private ?string $content = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    private ?\DateTimeInterface $publishedAt = null;
+    #[ORM\Column]
+    private ?\DateTimeImmutable $publishedAt = null;
 
     #[ORM\Column(length: 255)]
     private ?string $picture = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $author = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    public function __construct()
+    {
+        $this->publishedAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int { return $this->id; }
 
     public function getTitle(): ?string { return $this->title; }
-    public function setTitle(string $title): self { $this->title = $title; return $this; }
+    public function setTitle(string $title): static { $this->title = $title; return $this; }
 
     public function getContent(): ?string { return $this->content; }
-    public function setContent(string $content): self { $this->content = $content; return $this; }
+    public function setContent(string $content): static { $this->content = $content; return $this; }
 
-    public function getPublishedAt(): ?\DateTimeInterface { return $this->publishedAt; }
-    public function setPublishedAt(\DateTimeInterface $publishedAt): self { $this->publishedAt = $publishedAt; return $this; }
+    public function getPublishedAt(): ?\DateTimeImmutable { return $this->publishedAt; }
+    public function setPublishedAt(\DateTimeImmutable $publishedAt): static { $this->publishedAt = $publishedAt; return $this; }
 
     public function getPicture(): ?string { return $this->picture; }
-    public function setPicture(string $picture): self { $this->picture = $picture; return $this; }
+    public function setPicture(string $picture): static { $this->picture = $picture; return $this; }
+
+    public function getAuthor(): ?User { return $this->author; }
+    public function setAuthor(User $author): static { $this->author = $author; return $this; }
+
+    public function getCategory(): ?Category { return $this->category; }
+    public function setCategory(Category $category): static { $this->category = $category; return $this; }
 }
