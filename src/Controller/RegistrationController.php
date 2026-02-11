d209539e5a79bdce6686ebf519c406a763f115a1
mathieu <mathieu.gaudron@epitech.eu>
2026-02-11 14:53:35 +0100

    [ADD] register ok

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em
+    ): Response {
+        // Si déjà connecté, on peut rediriger
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = (string) $form->get('plainPassword')->getData();
+
+            $user->setPassword($passwordHasher->hashPassword($user, $plainPassword));
+            $user->setRoles(['ROLE_USER']); // sécurité
+
+            $em->persist($user);
+
+            try {
+                $em->flush();
+            } catch (\Throwable $e) {
+                // Cas le plus courant : email unique déjà pris
+                $this->addFlash('error', "Cet email est déjà utilisé.");
+                return $this->redirectToRoute('app_register');
+            }
+
+            $this->addFlash('success', 'Compte créé ! Tu peux te connecter.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
