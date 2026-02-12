5f766258b3f3422eb5f60f493f8a66b15b91d9aa
mathieu <mathieu.gaudron@epitech.eu>
2026-02-12 15:16:07 +0100

    [ADD] mise en page ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
-    {
+    public function index(
+        Request $request,
+        PostRepository $postRepository,
+        CategoryRepository $categoryRepository
+    ): Response {
+        $categoryId = $request->query->get('category'); // ex: "3" ou null
 
-        $posts = $postRepository->findBy([], ['publishedAt' => 'DESC']);
+        $criteria = [];
+        if ($categoryId) {
+            $criteria['category'] = (int) $categoryId;
+        }
+
+        $posts = $postRepository->findBy($criteria, ['publishedAt' => 'DESC']);
+        $categories = $categoryRepository->findBy([], ['name' => 'ASC']);
 
         return $this->render('home/index.html.twig', [
             'posts' => $posts,
+            'categories' => $categories,
+            'selectedCategory' => $categoryId,
         ]);
     }
 }
