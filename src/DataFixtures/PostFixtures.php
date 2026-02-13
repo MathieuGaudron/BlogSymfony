fb4c54ffe11e6539e71fa4392f01b1e59a60fcd0
mathieu <mathieu.gaudron@epitech.eu>
2026-02-13 16:00:58 +0100

    [FIX] fixtures

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -2,7 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use App\Entity\Post;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -15,34 +17,36 @@ class PostFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager): void
     {
-
+        // 1) DOFUS RETRO
         $p1 = new Post();
         $p1->setTitle('DOFUS RETRO');
         $p1->setContent('Screenshot sadida pandala');
         $p1->setPublishedAt(new \DateTimeImmutable('2026-02-12 09:05:31'));
         $p1->setPicture('Capture-d-ecran-2026-01-31-214723-698d97db1e81f.png');
-        $p1->setAuthor($this->getReference(UserFixtures::USER_PONCH));
-        $p1->setCategory($this->getReference(CategoryFixtures::CAT_JEU_VIDEO));
+        $p1->setAuthor($this->getReference(UserFixtures::USER_PONCH, User::class));
+        $p1->setCategory($this->getReference(CategoryFixtures::CAT_JEU_VIDEO, Category::class));
         $manager->persist($p1);
         $this->addReference(self::POST_1, $p1);
 
+        // 2) Mr. Canac BD
         $p2 = new Post();
         $p2->setTitle('Mr. Canac  BD');
         $p2->setContent('Le plus bg de sa géné toujours le chouchou des nanas');
         $p2->setPublishedAt(new \DateTimeImmutable('2026-02-12 13:34:49'));
         $p2->setPicture('Capture-d-ecran-2026-02-13-084407-698ed68e03c3e.png');
-        $p2->setAuthor($this->getReference(UserFixtures::USER_MELL));
-        $p2->setCategory($this->getReference(CategoryFixtures::CAT_PEOPLE));
+        $p2->setAuthor($this->getReference(UserFixtures::USER_MELL, User::class));
+        $p2->setCategory($this->getReference(CategoryFixtures::CAT_PEOPLE, Category::class));
         $manager->persist($p2);
         $this->addReference(self::POST_2, $p2);
 
+        // 3) Tawanchai ONE CHAMPIONSHIP
         $p3 = new Post();
         $p3->setTitle('Tawanchai ONE CHAMPIONSHIP');
         $p3->setContent('Tawanchai remporte la ceinture des -70kg Muay Thai au ONE CHAMPIONSHIP');
         $p3->setPublishedAt(new \DateTimeImmutable('2026-02-12 15:37:54'));
         $p3->setPicture('ceinture-tawanchai-698df3d258bfd.jpg');
-        $p3->setAuthor($this->getReference(UserFixtures::USER_PONCH));
-        $p3->setCategory($this->getReference(CategoryFixtures::CAT_SPORT));
+        $p3->setAuthor($this->getReference(UserFixtures::USER_PONCH, User::class));
+        $p3->setCategory($this->getReference(CategoryFixtures::CAT_SPORT, Category::class));
         $manager->persist($p3);
         $this->addReference(self::POST_3, $p3);
 
