f8627f947dd072e261a19763b2de281e0bf1c4ec
mathieu <mathieu.gaudron@epitech.eu>
2026-02-13 16:29:00 +0100

    [ADD]edit profil sans css

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ProfileType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,6 +25,7 @@ class ProfileController extends AbstractController
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
+        /** @var User $user */
         $user = $this->getUser();
 
         $posts = $postRepository->findBy(
@@ -40,6 +45,30 @@ class ProfileController extends AbstractController
 
         return $this->render('profile/infos.html.twig', [
             'user' => $this->getUser(),
+        ]);
+    }
+
+    #[Route('/profile/infos/edit', name: 'app_profile_infos_edit')]
+    public function editInfos(
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Infos mises à jour');
+            return $this->redirectToRoute('app_profile_infos');
+        }
+
+        return $this->render('profile/edit_infos.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
