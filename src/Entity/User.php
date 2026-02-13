ecf31ae30c177760de4dfae9c0864b3fe903c18c
mathieu <mathieu.gaudron@epitech.eu>
2026-02-13 14:58:35 +0100

    [FIX] userADMIN only new post

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
+    #[ORM\Column(length: 180, unique: true)] // ajout unique entity
     private ?string $email = null;
 
     #[ORM\Column(type: 'json')]
